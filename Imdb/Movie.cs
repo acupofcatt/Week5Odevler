ef9d8f6f11d28473ae13c3d04d62cf42dbbc6ae7
Elifgül Özgürbüz <115213915+acupofcatt@users.noreply.github.com>
2024-11-13 21:04:39 +0300

    Imdb eklendi

--- Imdb/Movie.cs
+++ Imdb/Movie.cs
@@ -1,12 +1,13 @@
 namespace Imdb;
 
+//isim ve puan bilgileri içeren bir film classı oluşturuyorum
 public class Movie
 {
     public string Name { get; set; }
     public double Rating { get; set; }
 
+    //default constructor
     public Movie()
     {
-        
     }
 }
--- Imdb/Program.cs
+++ Imdb/Program.cs
@@ -22,15 +22,21 @@ public class Program
 {
     public static void Main()
     {
+        //programa devam edip etmeme isteğini tanımlıyorum
         bool devam = false;
         List<Movie> movies = new List<Movie>();
         do
         {
+            //kullanıcıdan yeni bir film bilgisi alıyorum
             Console.WriteLine("Lütfen bir film ekleyiniz:");
+            
+            //film nesnesi oluşturan GetMovie metodunu çağırıorum
             movies.Add(GetMovie());
         
+            //kullanıcı devam etmek istiyor mu?
             Console.WriteLine("Yeni bir film eklemek için 'Evet' sonlandırmak için 'Hayır' giriniz.");
             string input = Console.ReadLine().ToLower();
+            //eğer evet girerse döngü devam etsin ve yeni film eklemeye devam etsin
             if (input == "evet")
             {
                 devam = true;
@@ -43,26 +49,27 @@ public class Program
         } while (devam == true);
         
 
+        //kullanıcının girdiği tüm filmleri yazdırıyorum
         Console.WriteLine("Girdiğiniz tüm filmler:");
         foreach (Movie movie in movies)
         {
             Console.WriteLine(movie.Name +" - "+ movie.Rating);
         }
 
+        //Imdb puanı 4 ve 9 arası olan filmleri yazdırıyorum
         Console.WriteLine("Imdb puanı 4 ve 9 arası olan filmler:");
         foreach (Movie movie in movies)
         {
-            //Imdb puanı 4 ve 9 arası olan filmleri yeni bir listeye taşıdım
             if (movie.Rating > 4 && movie.Rating < 9)
             {
                 Console.WriteLine(movie.Name +" - "+ movie.Rating);
             }
         }
 
+        //A ile başlayan filmleri yazdırıyorum
         Console.WriteLine("A ile başlayan filmler:");
         foreach (Movie movie in movies)
         {
-            //StartsWith metoduyla a harfi ile başlayan filmleri yeni bir listeye taşıdım
             if (movie.Name.StartsWith("A"))
             {
                 Console.WriteLine(movie.Name +" - "+ movie.Rating);
@@ -71,14 +78,20 @@ public class Program
         
     }
 
+    //Bu metot içinde Movie classından oluşturduğumuz nesneler için atamalar yapıyorum
     public static Movie GetMovie()
     {
+        //yeni bir nesne oluşturuyorum
         Movie movie = new Movie();
         
         Console.WriteLine("Lütfen film adı giriniz:");
+        
+        //Kullanıcının girdiği ismi ilk harfi büyük diğerleri küçük olacak şekilde düzenledim
+        //A ile başlayan filmleri yazdırırken de daha kolay oldu
         string nameIlk = Console.ReadLine().ToLower();
         string name = char.ToUpper(nameIlk[0]).ToString();
 
+        //kullanıcıdan aldığım ismi filmin ismine atadım
         movie.Name = name;
 
         Console.WriteLine("Lütfen film puanı giriniz:");
@@ -87,10 +100,12 @@ public class Program
         // Double'a dönüştürmeye çalış
         if (double.TryParse(ratingStr, out double rating))
         {
+            //kullanıcıdan aldığım sayıyı filmin puanına atadım
             movie.Rating = rating;
         }
         else
         {
+            //hata verdi
             Console.WriteLine("Hata: Geçersiz bir sayı girdiniz! 0 kabul edildi.");
             movie.Rating = 0;
         }
